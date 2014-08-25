e9175192c0b3a0babcd8db5d1b0f570284ca9e44
Sam Morris <mail@sammorrisdesign.co.uk>
2014-08-25 18:25:10 +0100

    Clean up api.js

--- api.js
+++ api.js
@@ -9,41 +9,38 @@ console.log(request);
 console.log(cheerio);
 
 app.get('/', function(requestvar, response) {
+    request({
+    "uri": url
+    }, function(err, resp, body){
+        var $ = cheerio.load(body);
 
-	request({
-	"uri": url
-	}, function(err, resp, body){
-		var $ = cheerio.load(body);
-		
-		// Create Structure for data
-		var output = {};
-		
-		// Target panel on Kerb website
-		$(".rota_panel > ul > li").each(function(index) {
-			output[index] = {};
-			output[index]["date"] = $(this).attr("rel");
-			
-			$(this).find("ul li").each(function(subIndex) {
-			
-				subIndex = "stand-" + subIndex;
-				output[index][subIndex] = {};
-				output[index][subIndex]["standName"] = $(this).find("h4 a").text();
-				output[index][subIndex]["href"] = $(this).find("h4 a").attr("href");
-				output[index][subIndex]["image"] = $(this).find("a > img").attr("src");
-				output[index][subIndex]["description"] = $(this).find("p").text();
-				
-			});
-			
-		});
-		
-	// Convert to json
-	output = JSON.stringify(output, null, 4);
-	response.send(output);
-	});
+        // Create Structure for data
+        var output = {};
 
+        // Target panel on Kerb website
+        $(".rota_panel > ul > li").each(function(index) {
+            output[index] = {};
+            output[index]["date"] = $(this).attr("rel");
+
+            $(this).find("ul li").each(function(subIndex) {
+
+                subIndex = "stand-" + subIndex;
+                output[index][subIndex] = {};
+                output[index][subIndex]["standName"] = $(this).find("h4 a").text();
+                output[index][subIndex]["href"] = $(this).find("h4 a").attr("href");
+                output[index][subIndex]["image"] = $(this).find("a > img").attr("src");
+                output[index][subIndex]["description"] = $(this).find("p").text();
+
+            });
+        });
+
+    // Convert to json
+    output = JSON.stringify(output, null, 4);
+    response.send(output);
+    });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
