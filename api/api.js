9f0ac1024e91dfe1eb3978ef56c255b86052c1f5
Sam Morris <mail@sammorrisdesign.co.uk>
2014-08-25 21:05:52 +0100

    Use Handlebars output primative html

--- api/api.js
+++ api/api.js
@@ -1,16 +1,20 @@
 // Required Modules
 var request = require('request');
 var cheerio = require('cheerio');
+var handlebars = require("handlebars");
 var fs = require("fs");
 
 // External Sources
+require.extensions['.html'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
+var html = require("../template/template.html");
 var descriptions = require("./descriptions.json");
 var url = "http://www.kerbfood.com/kings-cross/";
 
-console.log(request);
-console.log(cheerio);
-console.log(descriptions);
-console.log(descriptions["bbq-lab"].length);
+// console.log(request);
+// console.log(cheerio);
+// console.log(descriptions);
 
 request({
 "uri": url
@@ -23,31 +27,32 @@ request({
 
     // Target panel on Kerb website
     $(".rota_panel > ul > li").each(function(index) {
+        index = "date-" + index;
         output[index] = {};
         output[index]["date"] = $(this).attr("rel");
+        output[index] = [];
 
         $(this).find("ul li").each(function(subIndex) {
             var handle = $(this).find("h4 a").attr("href").replace("/traders/", "").replace("/", "");
-            subIndex = "stand-" + subIndex;
-            output[index][subIndex] = {};
-            output[index][subIndex]["handle"] = handle;
-            output[index][subIndex]["standName"] = $(this).find("h4 a").text().replace(" (inKERBating)", "").replace(" - Seychelles Kitchen", "");
+            var seller = {};
+            seller["handle"] = handle;
+            seller["standName"] = $(this).find("h4 a").text().replace(" (inKERBating)", "").replace(" - Seychelles Kitchen", "");
             if (descriptions[handle] != void(0)) {
-                output[index][subIndex]["description"] = descriptions[handle];
+                seller["description"] = descriptions[handle];
             } else {
-                output[index][subIndex]["description"] = $(this).find("p").text();
+                seller["description"] = $(this).find("p").text();
             }
-            output[index][subIndex]["href"] = $(this).find("h4 a").attr("href");
-            output[index][subIndex]["image"] = $(this).find("a > img").attr("src");
-
+            seller["href"] = $(this).find("h4 a").attr("href");
+            seller["image"] = $(this).find("a > img").attr("src");
+            output[index].push(seller);
         });
     });
 
-    // Convert to json
-    output = JSON.stringify(output, null, 4);
-
+    // Compile html using template.html
+    var template = handlebars.compile(html);
+    var result = template(output);
     // Output file
-    fs.writeFile("api.json", output, function(err) {
+    fs.writeFile("../index.html", result, function(err) {
         if (err) {
             console.log(err);
         } else {
