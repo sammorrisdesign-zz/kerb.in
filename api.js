90b2fc5e0c6d55cbd12778e0b327e88eb6ee9799
Sam Morris <mail@sammorrisdesign.co.uk>
2014-08-25 18:48:18 +0100

    No longer require http request

--- api.js
+++ api.js
@@ -9,44 +9,41 @@ var url = "http://www.kerbfood.com/kings-cross/";
 console.log(request);
 console.log(cheerio);
 
-app.get('/', function(requestvar, response) {
-    request({
-    "uri": url
-    }, function(err, resp, body){
-        var $ = cheerio.load(body);
+request({
+"uri": url
+}, function(err, resp, body){
+    var $ = cheerio.load(body);
 
-        // Create Structure for data
-        var output = {};
+    // Create Structure for data
+    var output = {};
 
-        // Target panel on Kerb website
-        $(".rota_panel > ul > li").each(function(index) {
-            output[index] = {};
-            output[index]["date"] = $(this).attr("rel");
+    // Target panel on Kerb website
+    $(".rota_panel > ul > li").each(function(index) {
+        output[index] = {};
+        output[index]["date"] = $(this).attr("rel");
 
-            $(this).find("ul li").each(function(subIndex) {
+        $(this).find("ul li").each(function(subIndex) {
 
-                subIndex = "stand-" + subIndex;
-                output[index][subIndex] = {};
-                output[index][subIndex]["standName"] = $(this).find("h4 a").text().replace(" (inKERBating)", "");
-                output[index][subIndex]["href"] = $(this).find("h4 a").attr("href");
-                output[index][subIndex]["image"] = $(this).find("a > img").attr("src");
-                output[index][subIndex]["description"] = $(this).find("p").text();
+            subIndex = "stand-" + subIndex;
+            output[index][subIndex] = {};
+            output[index][subIndex]["standName"] = $(this).find("h4 a").text().replace(" (inKERBating)", "");
+            output[index][subIndex]["href"] = $(this).find("h4 a").attr("href");
+            output[index][subIndex]["image"] = $(this).find("a > img").attr("src");
+            output[index][subIndex]["description"] = $(this).find("p").text();
 
-            });
         });
+    });
 
-        // Convert to json
-        output = JSON.stringify(output, null, 4);
-        response.send(output);
-        
-        // Output file
-        fs.writeFile("api.json", output, function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("File Written");
-            }
-        });
+    // Convert to json
+    output = JSON.stringify(output, null, 4);
+
+    // Output file
+    fs.writeFile("api.json", output, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("File Written");
+        }
     });
 });
 
