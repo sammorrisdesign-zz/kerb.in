b47e1f72657d257cff9b1e05a243363d13b7ca62
Sam Morris <mail@sammorrisdesign.co.uk>
2014-08-31 22:43:08 +0100

    Add description and tomorrow's menu

--- api/api.js
+++ api/api.js
@@ -32,6 +32,7 @@ request({
         output[index] = {};
         output[index]["date"] = $(this).attr("rel");
         output[index] = [];
+        var numOfTraders = $(this).find("ul li").length;
 
         $(this).find("ul li").each(function(subIndex) {
             var handle = $(this).find("h4 a").attr("href").replace("/traders/", "").replace("/", "");
@@ -45,6 +46,9 @@ request({
             }
             seller["href"] = $(this).find("h4 a").attr("href");
             seller["image"] = $(this).find("a > img").attr("src");
+            if (numOfTraders - 1 == subIndex) {
+                seller["last"] = true;
+            }
             output[index].push(seller);
         });
     });
